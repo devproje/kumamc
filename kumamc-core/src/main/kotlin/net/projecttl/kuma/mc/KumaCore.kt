c07b14df06e147d9ea0acfcff374496789d11099
Project_IO <me@projecttl.net>
2023-03-06 16:01:15 +0900

    feat: middle save #1

--- kumamc-core/src/main/kotlin/net/projecttl/kuma/mc/KumaCore.kt
+++ kumamc-core/src/main/kotlin/net/projecttl/kuma/mc/KumaCore.kt
@@ -1,2 +1,18 @@
 package net.projecttl.kuma.mc
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import net.minestom.server.MinecraftServer
+
+class KumaCore {
+    private val server = MinecraftServer.init()
+
+
+    suspend fun run() {
+        coroutineScope {
+            launch {
+
+            }
+        }
+    }
+}
